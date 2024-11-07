439e7bae58a0f9186d75edf1cc36ec605548d114
SimHB <hyeon8967@gmail.com>
2024-11-07 15:07:58 +0900

    삭제

--- runningON/src/main/java/com/ict/runningON/dao/PostsDAO.java
+++
@@ -1,7 +0,0 @@
-package com.ict.runningON.dao;
-
-import com.ict.runningON.vo.PostsVO;
-
-public interface PostsDAO {
-	public int getPostsInsert(PostsVO pvo);
-}
--- runningON/src/main/java/com/ict/runningON/service/PostsService.java
+++
@@ -1,7 +0,0 @@
-package com.ict.runningON.service;
-
-import com.ict.runningON.vo.PostsVO;
-
-public interface PostsService {
-	public int getPostsInsert(PostsVO pvo);
-}
